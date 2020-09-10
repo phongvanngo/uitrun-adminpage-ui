6f1191472d481949c49110943045663944676c74
phongvanngo <19520834@gm.uit.edu.vn>
2020-09-10 11:32:27 +0700

    hello

---
+++ packages/isomorphic/src/containers/test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Test(props) {
+  return <div>hello</div>;
+}
+
+export default Test;
--- packages/isomorphic/src/router.js
+++ packages/isomorphic/src/router.js
@@ -7,6 +7,7 @@ import {
   useLocation,
 } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import Test from './containers/test';
 
 import ErrorBoundary from './ErrorBoundary';
 import { PUBLIC_ROUTE } from './route.constants';
@@ -17,7 +18,8 @@ const publicRoutes = [
   {
     path: PUBLIC_ROUTE.LANDING,
     exact: true,
-    component: lazy(() => import('@iso/containers/Pages/SignIn/SignIn')),
+    component: lazy(() => import('./containers/test')),
+    // component: lazy(() => import('@iso/containers/Pages/SignIn/SignIn')),
   },
   {
     path: PUBLIC_ROUTE.SIGN_IN,
